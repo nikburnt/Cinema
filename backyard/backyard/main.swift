5b6c8ebd26ddbff927ea0d89f57caefa4778ebef
Nik Burnt <nikburnt@gmail.com>
2020-05-08 18:18:39 +0300

    backend part setting up

---
+++ backyard/backyard/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  backyard
+//
+//  Created by Nik Burnt on 5/8/20.
+//  Copyright © 2020 Nik Burnt Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
